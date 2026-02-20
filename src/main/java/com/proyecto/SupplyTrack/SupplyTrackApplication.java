52bec81bbb92c2e79bb6a351ebf8df732cc1f2ff
sebasgom152 <sgomez80388@ufide.ac.cr>
2026-02-19 20:25:53 -0600

    t push -u origen mainprimer commit

---
+++ src/main/java/com/proyecto/SupplyTrack/SupplyTrackApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.SupplyTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/SupplyTrack/SupplyTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.SupplyTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
